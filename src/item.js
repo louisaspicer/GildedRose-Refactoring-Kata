015195c125b885394df566a7ee0d658dbde664e3
Louisa <louisaspicer@gmail.com>
2017-01-25 11:12:44 +0000

    Passes both item edge cases

--- src/item.js
+++ src/item.js
@@ -1,6 +1,8 @@
 function Item(name, sellIn, quality) {
   if (sellIn < 1) {
     throw new Error("'Sell In' number cannot be less than 1");
+  } else if (quality > 50) {
+    throw new Error("'Quality' cannot be more than 50");
   }
   this.name = name;
   this.sellIn = sellIn;
